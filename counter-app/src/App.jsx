c89093d5954f52e977fc490c4dbebae9c503add5
Phùng Văn Tú <tuphung.03032004@gmail.com>
2025-04-16 10:41:45 +0700

    Counter App - Redux toolkit

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,9 +1,13 @@
-import CounterRedux from "./CounterRedux";
+import { Provider } from "react-redux";
+import CounterReduxToolkit from "./CounterReduxToolkit";
+import toolkitStore from "./redux/toolkitStore";
 
 function App() {
   return (
-    <div className="flex justify-center items-center min-h-screen p-4">
-      <CounterRedux />
+    <div className="flex flex-col justify-center items-center min-h-screen p-4 gap-8">
+      <Provider store={toolkitStore}>
+        <CounterReduxToolkit />
+      </Provider>
     </div>
   );
 }
